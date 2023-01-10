8ec60dd000ea19cfa744abeb93881495759ded5e
Emilio Kariuki <emilio113kariuki@gmail.com>
2023-01-10 21:39:47 +0300

    ADD added cors

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 import reviewRouter from "..//Routes/Review/review.js";
 import bidRouter from "..//Routes/Bid/bid.js";
 import jobRouter from "../Routes/Job/job.js";
-// import { createUser, signIn } from "../Routes/Authentication/User/user.js";
+import { createEmployee,createEmployer,signInEmployee,signInEmployer} from "../Routes/Authentication/User/user.js";
 import { protect } from "../Routes/Authentication/auth.js";
 
 dotenv.config();
@@ -13,6 +13,22 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+app.use(cors(
+  {
+      origin: '*',
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      allowedHeaders: ['Content-Type', 'Authorization']
+  }
+))
+
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -24,8 +40,12 @@ app.all("/", (req, res) => {
 
 app.use("/review", reviewRouter);
 app.use("/job", jobRouter);
-// app.use("/login", signIn);
-// app.use("/", createUser);
+
+app.use("/login/employee", signInEmployee);
+app.use("/login/employer", signInEmployer);
+app.use("/register/employee", createEmployee);
+app.use("/register/employer", createEmployer);
+
 app.use("/bid", bidRouter);
 
 export const start = () => {
