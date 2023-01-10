c9806101ab633c448905d40302b34723e7c6567e
Emilio Kariuki <emilio113kariuki@gmail.com>
2023-01-10 17:49:34 +0300

    ADD added the bid route

---
+++ Routes/Bid/bid.js
@@ -0,0 +1,45 @@
+import {Router} from 'express';
+import {body} from 'express-validator';
+import {prisma} from '../Database/db.js';
+
+
+const router = Router();
+
+router.post('/', async (req,res)=>{
+    try{
+        const bid = await prisma.bid.create({
+            data:{
+                name: req.body.name,
+                description: req.body.description,
+                belongToJob: req.body.id
+            },
+        })
+        if(!bid){
+            throw new Error('Bid could not be created')
+        }
+
+        res.json({message:'Bid created successfully'})
+    }catch(e){
+        res.status(500).json({message:e.message})
+    }
+})
+
+router.get('/:id', async(req,res)=>{
+    try{
+        const bids = await prisma.bid.findMany({
+            where:{
+                belongToJob: parseInt(req.params.id)
+            },
+
+        })
+        if(!bids){
+            throw new Error('Bids could not be fetched')
+        }
+        res.json(bids)
+
+    }catch(e){
+        res.status(500).json({message:e.message})
+    }
+})
+
+export default router;
